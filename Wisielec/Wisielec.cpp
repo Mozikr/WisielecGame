187b073c3af420eb714ead64b9c7e719eba560b9
Szymon Mozol <szymonmozol123@gmail.com>
2024-07-15 15:32:56 +0200

    Final

--- Wisielec/Wisielec.cpp
+++ Wisielec/Wisielec.cpp
@@ -1,20 +1,98 @@
-﻿// Wisielec.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <cctype>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void loadWords(const string& filename, vector<string>& words) {
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Nie mozna otworzyc pliku: " << filename << endl;
+        exit(1);
+    }
+
+    string word;
+    while (file >> word) {
+        words.push_back(word);
+    }
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+string selectRandomWord(const vector<string>& words) {
+    srand(static_cast<unsigned int>(time(0)));
+    int randomIndex = rand() % words.size();
+    return words[randomIndex];
+}
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+void displayCurrentProgress(const string& word, const string& guessed) {
+    for (char ch : word) {
+        if (guessed.find(ch) != string::npos) {
+            cout << ch << ' ';
+        }
+        else {
+            cout << "_ ";
+        }
+    }
+    cout << endl;
+}
+
+bool isWordGuessed(const string& word, const string& guessed) {
+    for (char ch : word) {
+        if (guessed.find(ch) == string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    vector<string> words;
+    loadWords("words.txt", words);
+
+    string word = selectRandomWord(words);
+    string guessed;
+    int maxAttempts = 6;
+    int wrongAttempts = 0;
+
+    cout << "Gra w wisielca!" << endl;
+
+    while (wrongAttempts < maxAttempts) {
+        cout << "Slowo: ";
+        displayCurrentProgress(word, guessed);
+
+        char guess;
+        cout << "Podaj litere: ";
+        cin >> guess;
+        guess = tolower(guess);
+
+        if (guessed.find(guess) != string::npos) {
+            cout << "Ta litera juz zostala podana!" << endl;
+            continue;
+        }
+
+        guessed += guess;
+
+        if (word.find(guess) == string::npos) {
+            wrongAttempts++;
+            cout << "Zla litera! Pozostalo prob: " << (maxAttempts - wrongAttempts) << endl;
+        }
+        else {
+            cout << "Dobra litera!" << endl;
+        }
+
+        if (isWordGuessed(word, guessed)) {
+            cout << "Gratulacje! Odgadles slowo: " << word << endl;
+            break;
+        }
+    }
+
+    if (wrongAttempts == maxAttempts) {
+        cout << "Przegrales! Slowo to: " << word << endl;
+    }
+
+    return 0;
+}
